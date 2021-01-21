c2a68b0c3f07c65157c297d053bf9ff3307b49fd
Sejin Kim <jeen0202@korea.ac.kr>
2021-01-21 18:22:40 +0900

    url의 path에 따른 Not found 출력 적용

--- Day3/main.js
+++ Day3/main.js
@@ -4,38 +4,39 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
+    var pathname = url.parse(_url,true).pathname;
     var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
-    fs.readFile(`../data/${title}`,'utf8',function(err,descrpition){
+    
+    //입력에 따른 반응 추가
+    if(pathname == '/'){
+      fs.readFile(`../data/${title}`,'utf8',function(err,descrpition){
        var template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ul>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ul>
-  <h2>${title}</h2>
-  <p>${descrpition}</p>
-</body>
-</html>
-  `;
-  response.end(template);
-    })
-});
+     <!doctype html>
+ <html>
+ <head>
+   <title>WEB1 - ${title}</title>
+   <meta charset="utf-8">
+ </head>
+ <body>
+   <h1><a href="/">WEB</a></h1>
+   <ul>
+     <li><a href="/?id=HTML">HTML</a></li>
+     <li><a href="/?id=CSS">CSS</a></li>
+     <li><a href="/?id=JavaScript">JavaScript</a></li>
+   </ul>
+   <h2>${title}</h2>
+   <p>${descrpition}</p>
+ </body>
+ </html>
+   `;
+   response.writeHead(200);
+  response.end(template); 
+  });    
+  }else{
+    response.writeHead(200);
+    response.end('Not found');    
+  }
+ });
+
+
 app.listen(3000);
