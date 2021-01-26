baa62ad6a780499c92382eb4d5135c49570ff2da
Sejin Kim <jeen0202@korea.ac.kr>
2021-01-26 22:02:23 +0900

    mysql을 이용해 CRUD 구현 완료

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
   host:'localhost',
   user:'root',
-  password: '',
+  password: '111111',
   database : 'tutorials'
 });
 conn.connect();
@@ -44,13 +44,10 @@ var app = http.createServer(function(request,response){
             conn.query(`select * from topic where id=?`,[queryData.id],function(error2,topic){
               if(error2){
                 throw error2;
-              }
-              console.log(topic[0].title);
-              var title = topic[0].title;
-              var descrpition = topic[0].description;
+              }          
               var list = template.list(topics);
-              var html = template.HTML(title,list
-              ,`<h2>${title}</h2>${descrpition}`
+              var html = template.HTML(topic[0].title,list
+              ,`<h2>${topic[0].title}</h2>${topic[0].description}`
               ,`<a href="/create">create</a>
                     <a href="/update?id=${queryData.id}">update</a>
                     <form action = "/delete_process" method = "post">
@@ -102,28 +99,32 @@ var app = http.createServer(function(request,response){
       })
     });   
   }else if(pathname === "/update"){
-    fs.readdir('./data', function(error,filelist){         
-      fs.readFile(`./data/${filterdId}`,'utf8',function(err,descrpition){
-       var title = queryData.id;
-       var list = template.list(filelist);
-       var html = template.HTML(title,list
-         ,`<form action="/update_process" method="post">
-          <input type = "hidden" name="id" value="${title}">
-         <p><input type ="text" name="title" placeholder ="title" value="${title}"></p>
-         <p>
-           <textarea name="description" placeholder="description">${descrpition}</textarea>
-        </p>
-         <p>
-           <input type="submit">    
-         </p>
-       </form> 
-         `
-         ,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-         );
-     response.writeHead(200);
-     response.end(html); 
-     });  
-   });
+  conn.query (`select * from topic`,function(error,topics){
+    if(error){
+      throw error;
+    }
+    conn.query(`select * from topic where id=?`,[queryData.id],function(error2,topic){
+      if(error2){
+        throw error2;
+      }      
+      var list = template.list(topics);
+      var html = template.HTML(topic[0].title,list
+      ,`<form action="/update_process" method="post">
+          <input type = "hidden" name="id" value="${topic[0].id}">
+            <p><input type ="text" name="title" placeholder ="title" value="${topic[0].title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+            </p>
+            <p>
+              <input type="submit">    
+             </p>
+           </form> `
+      ,`<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+      );              
+      response.writeHead(200);
+      response.end(html); 
+    });
+  }) 
   }else if(pathname === "/update_process"){
     var body = "";
     //post방식으로 파편화 된 data가 수신될 때마다 callback함수를 실행한다.(Data event)
@@ -133,16 +134,15 @@ var app = http.createServer(function(request,response){
     });
     //정보의 수신이 끝났을 경우(end event)
     request.on("end",function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`,`data/${title}`, function(error){
-        fs.writeFile(`data/${title}`,description,'utf8',
-        function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end(); 
-        })       
+      var post = qs.parse(body); 
+      conn.query(`
+      update topic set title = ?,description = ?,author_id=1 where id =?`,
+      [post.title,post.description,post.id],
+      function(error,result){
+        if(error){
+          throw error;
+        }response.writeHead(302, {Location: `/?id=${post.id}`});
+        response.end(); 
       })
     });  
   }else if(pathname === "/delete_process"){
@@ -154,8 +154,14 @@ var app = http.createServer(function(request,response){
       var post = qs.parse(body);
       var id = post.id;
       var filterdId = path.parse(id).base;     
-      fs.unlink(`data/${filterdId}`, function(error){
-        response.writeHead(302, {Location: `/`});
+      // fs.unlink(`data/${filterdId}`, function(error){
+      //   response.writeHead(302, {Location: `/`});
+      //   response.end(); 
+      // })
+      conn.query(`delete from topic where id = ?`,[post.id],function(error,result){
+        if(error){
+          throw error;
+        }response.writeHead(302, {Location: `/`});
         response.end(); 
       })
     }); 
