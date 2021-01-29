77b3fc25a8cac6f399720c7b720e280d50b34408
Sejin Kim <jeen0202@korea.ac.kr>
2021-01-29 14:24:45 +0900

    express.js를 활용하여 author table CRUD  구현

--- main.js
+++ main.js
@@ -1,87 +1,25 @@
 //express.js 호출
 const express = require('express')
-var topic = require('./lib/topic');
-var author = require('./lib/author');
-//express는 
 const app = express();
 const port = 3000;
+//사용자 모듈 호출
+var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 //route 기능 수행
-app.get('/', (req, res) => {
-  topic.home(req, res);
-})
-app.get('/page/:pageId', (req, res) => {
-  topic.page(req,res);   
-})
-app.get('/create',(req,res)=> {
-  topic.create(req,res);
-})
-app.post('/create_process',(req,res)=>{
-  topic.create_process(req,res);
-})
-app.get('/update/:pageId',(req,res) =>{
-  topic.update(req,res);
-})
-app.post('/update_process',(req,res) =>{
-  topic.update_process(req,res);
-})
-app.post('/delete_process',(req,res) =>{
-  topic.delete_process(req,res);
-})
+//topic 관련페이지 호출
+app.get('/', (req, res) =>{topic.home(req, res);})
+app.get('/page/:pageId', (req, res) => {topic.page(req,res);})
+app.get('/create',(req,res)=> {topic.create(req,res);})
+app.post('/create_process',(req,res)=>{topic.create_process(req,res);})
+app.get('/update/:pageId',(req,res) =>{topic.update(req,res);})
+app.post('/update_process',(req,res) =>{topic.update_process(req,res);})
+app.post('/delete_process',(req,res) =>{topic.delete_process(req,res);})
+//author 관련 페이지 호출
+app.get('/author',(req,res) => {author.home(req,res);})
+app.post('/author/create_process', (req,res) =>{author.create_process(req,res);})
+app.get(`/author/update/:authorId`, (req,res) =>{author.update(req,res);})
+app.post('/author/update_process',(req,res)=>{author.update_process(req,res);})
+app.post("/author/delete_process", (req,res)=>{author.delete_process(req,res);})
 
-//위의 코드와 같은 기능을 수행
-// app.get('/', function(req,res){
-//   return res.send('Hello World!')
-// });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-/*
-var http = require('http');
-var url = require('url');
-
-//사용자 모듈
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url,true).query;
-    var pathname = url.parse(_url,true).pathname;      
-    if(pathname === '/'){
-      //id값이 없는 최초의 페이지에 대한 부분 모듈화
-      if(queryData.id === undefined){
-        topic.home(request, response);              
-      }else{
-        topic.page(request,response);
-                
-    }       
-  }else if(pathname ==="/create"){   
-    topic.create(request,response);
-  }else if (pathname==="/create_process"){
-    topic.create_process(request, response);
-  }else if(pathname === "/update"){
-    topic.update(request, response);
-  }else if(pathname === "/update_process"){
-    topic.update_process(request,response);
-  }else if(pathname === "/delete_process"){
-    topic.delete_process(request,response);
-  }else if(pathname === "/author"){
-    author.home(request,response);
-  }
-  else if(pathname === "/author/create_process"){
-    author.create_process(request,response);
-  }else if(pathname === "/author/update"){
-    author.update(request,response);
-  }else if(pathname === "/author/update_process"){
-    author.update_process(request,response);
-  }else if(pathname === "/author/delete_process"){
-    author.delete_process(request,response);
-  }
-  else{
-    response.writeHead(404);
-    response.end('Not found');    
-  }
- });
-
-
-app.listen(3000);
-*/
+app.listen(port, () => {console.log(`listening at port ${port}`) })
