9200d25da9b8f8565529f86728b1428ea7065de1
Sejin Kim <jeen0202@korea.ac.kr>
2021-01-29 15:32:12 +0900

    compression을 사용하여 웹 압축

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ const port = 3000;
 var topic = require('./lib/topic');
 var author = require('./lib/author');
 //미들웨어 호출
+//post방식에서 body를 검출해 주는 body-parser
 var bodyParser = require('body-parser');
+//압축을 통해 서버에서 전송하는 data의 size를 줄여주는 compression
+var compression = require('compression')
 //미들웨어 실행
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(compression());
 //route 기능 수행
 //topic 관련페이지 호출
 app.get('/', (req, res) =>{topic.home(req, res);})
