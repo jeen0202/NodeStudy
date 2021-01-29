72c9deb57477b5209f2ffb449c8d7d588c64d5e5
Sejin Kim <jeen0202@korea.ac.kr>
2021-01-29 17:42:38 +0900

    주소체계 정리

--- main.js
+++ main.js
@@ -25,12 +25,12 @@ app.get("/author*",readdb.author);
 //route 기능 수행
 //topic 관련페이지 호출
 app.get('/', (req, res) =>{topic.home(req, res);})
-app.get('/page/:pageId', (req, res) => {topic.page(req,res);})
-app.get('/create',(req,res)=> {topic.create(req,res);})
-app.post('/create_process',(req,res)=>{topic.create_process(req,res);})
-app.get('/update/:pageId',(req,res) =>{topic.update(req,res);})
-app.post('/update_process',(req,res) =>{topic.update_process(req,res);})
-app.post('/delete_process',(req,res) =>{topic.delete_process(req,res);})
+app.get('/topic/create',(req,res)=> {topic.create(req,res);})
+app.get('/topic/:pageId', (req, res) => {topic.page(req,res);})
+app.post('/topic/create_process',(req,res)=>{topic.create_process(req,res);})
+app.get('/topic/update/:pageId',(req,res) =>{topic.update(req,res);})
+app.post('/topic/update_process',(req,res) =>{topic.update_process(req,res);})
+app.post('/topic/delete_process',(req,res) =>{topic.delete_process(req,res);})
 //author 관련 페이지 호출
 app.get('/author/',(req,res) => {author.home(req,res);})
 app.post('/author/create_process', (req,res) =>{author.create_process(req,res);})
