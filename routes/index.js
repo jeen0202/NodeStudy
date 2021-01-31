a17df4d8ba79da9563df5342e6a52031ee2e0e25
Sejin Kim <jeen0202@korea.ac.kr>
2021-01-31 15:15:33 +0900

    Cookie를 사용한 인증기능 구현

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ const topic = require('../lib/topic');
 router.get('/', (req, res) =>{topic.home(req, res);})
 router.get('/login',(req,res)=>{topic.login(req,res);})
 router.post('/login_process',(req,res)=>{topic.login_process(req,res);})
+router.get('/logout_process',(req,res)=>{topic.logout_process(req,res);})
 module.exports  = router;
