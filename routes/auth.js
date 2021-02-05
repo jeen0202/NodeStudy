debc56d6fa5fa1050fb8ccec64b22d0818f2107a
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-05 13:30:10 +0900

    facebook 연동 로그인 구현

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,19 @@ module.exports = function(passport){
   router.get('/google',
   passport.authenticate('google', { 
     scope: ['https://www.googleapis.com/auth/plus.login','email']
-  }));
+  }));  
   router.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/auth/login' }),
   function(req, res) {
     res.redirect('/');
   });
+  router.get('/facebook', passport.authenticate('facebook',{
+      scope:'email'
+  }));
+  router.get('/facebook/callback',
+  passport.authenticate('facebook', 
+  { successRedirect: '/',
+  failureRedirect: '/auth/login' }));
   router.get('/login',(req,res)=>{auth.login(req,res);})
   //router.post('/login_process',(req,res)=>{auth.login_process(req,res);})
   //File-Store를 사용했기때문에 바로 redirect하지 않고 save를 통해 저장후 redirect하자  
